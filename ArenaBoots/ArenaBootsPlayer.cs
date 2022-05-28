c8d4d120bf96f402254f84bfac850c1695122e90
Tuncay Yildiz <tuncay.yildiz@live.de>
2022-05-28 02:23:58 +0200

    Initial commit

---
+++ ArenaBoots/ArenaBootsPlayer.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.GameInput;
+using Terraria.ModLoader;
+using RhosRequests.RandomStuff;
+
+namespace RhosRequests.ArenaBoots
+{
+    public class ArenaBootsPlayer : ModPlayer
+    {
+        public bool isOn = false;
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            if (KeybindSystem.ToggleArenaBoots.JustPressed)
+            {
+                if (isOn == false)
+                {
+                    isOn = true;
+                    Main.NewText($"Arena Boots enabled");
+                }
+                else
+                {
+                    isOn = false;
+                    Main.NewText($"Arena Boots disabled");
+                }
+            }
+        }
+
+    }
+}
